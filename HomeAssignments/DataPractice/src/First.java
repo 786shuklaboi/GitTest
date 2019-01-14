e1c231629604f4e02031fd8fb04a3207a902dde4
Akash <akashshukla3012@gmail.com>
2019-01-14 21:06:51 +0530

    HomeAssig

---
+++ HomeAssignments/DataPractice/src/First.java
@@ -0,0 +1,6 @@
+class First
+{public static void main(String args[])
+{int num1=85,num2=10;
+    System.out.println("addition of two integers num1 and num2 are: ");
+System.out.println(num1+num2);
+}}
